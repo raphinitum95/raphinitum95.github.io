c3ec5dcff8faf817d0562f7429ac99f200691a1f
raphinitum95 <raphaeldlst@gmail.com>
2020-08-22 15:49:34 -0500

    Updates

---
+++ precache-manifest.924420019b46f0b4a7c298d772cebc04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f5bd96f20a6917036ae440b1645ef66",
+    "url": "/serranos-mexin/index.html"
+  },
+  {
+    "revision": "40a298c7b2d3c776367d",
+    "url": "/serranos-mexin/static/css/main.d90cda22.chunk.css"
+  },
+  {
+    "revision": "c4b951e9e09cb9da11f4",
+    "url": "/serranos-mexin/static/js/2.290108b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/serranos-mexin/static/js/2.290108b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40a298c7b2d3c776367d",
+    "url": "/serranos-mexin/static/js/main.8f9a32c2.chunk.js"
+  },
+  {
+    "revision": "b3c4d80dab524c07f3d2",
+    "url": "/serranos-mexin/static/js/runtime-main.1510db02.js"
+  },
+  {
+    "revision": "977a472b0caa5a6e67f1a50c71c93b95",
+    "url": "/serranos-mexin/static/media/Alamo.977a472b.jpg"
+  },
+  {
+    "revision": "9f9fdca7081e79ae83d563cae8ef5334",
+    "url": "/serranos-mexin/static/media/Logo-Transparent.9f9fdca7.png"
+  }
+]);
